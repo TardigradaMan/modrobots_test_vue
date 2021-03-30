3906f56fcf7675188117436e3945ca7bce0ccc72
TardigradaMan <52483460+TardigradaMan@users.noreply.github.com>
2021-03-30 07:35:50 +0300

    Update range.js

--- src/utils/range.js
+++ src/utils/range.js
@@ -2,8 +2,4 @@ export const range = (start, end) => {
   return [...Array(end).keys()].map(p => p + start)
 }
 
-// const range = (start, end) => {
-//   return [...Array(end).keys()].map(p => p + start)
-// }
 
-// console.log(range(980, 1040))
